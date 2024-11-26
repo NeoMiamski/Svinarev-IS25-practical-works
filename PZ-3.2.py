5d73a4e7dbef8e272de3a2dbe0e8e82f655d17a7
NeoMiamski <miamski.work@gmail.com>
2024-11-26 10:35:34 +0300

    Add files via upload

--- PZ-3.2.py
+++ PZ-3.2.py
@@ -1,9 +1,8 @@
-
 x1 = int(input('¬ведите координаты x1: '))
 y1 = int(input('¬ведите координаты y1: '))
 x2 = int(input('¬ведите координаты x2: '))
 y2 = int(input('¬ведите координаты y2: '))
-if (1 <= x2 <= 8 and y1 == y2) or (1 <= y2 <= 8 and x1 == x2):
+if (1 <= x2 <= 8 and y1 == y2) or (1 <= y2 <= 8 and x1 == x2): # проверка
     print("True")
 else:
     print("False")
