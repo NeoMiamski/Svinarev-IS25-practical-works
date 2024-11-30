f46a2cfec849c544a7f722fb822002e160de6e19
NeoMiamski <miamski.work@gmail.com>
2024-11-30 11:07:12 +0300

    Add files via upload

---
+++ PZ-4.1.py
@@ -0,0 +1,9 @@
+A = int(input("Введите число: "))
+B = int(input("Введите число, больше предыдушего: "))
+for i in range(A, B + 1): # Запуск цикла
+    if i == A + 1:
+        print(i)
+        print(i)
+    else:
+        print(i)
+
