a41a6dd1bb5cec3fcc05b39c41bef0bf87d5a5eb
NeoMiamski <miamski.work@gmail.com>
2024-11-30 19:15:14 +0300

    Update PZ-4.2.py

--- PZ-4.2.py
+++ PZ-4.2.py
@@ -1,7 +1,7 @@
 N = int(input("Введите делимое: "))
 K = int(input("Введите делитель: "))
 Quotient = 0
-while N >= K:
+while N >= K: #Деление в формате цикла
     N = N - K
     Quotient += 1
 Remainder = N
