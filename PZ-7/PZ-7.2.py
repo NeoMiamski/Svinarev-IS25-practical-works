375c7b174d619b6687a17030347d0ae6bc42bcb4
NeoMiamski <miamski.work@gmail.com>
2025-02-15 11:56:28 +0300

    Add files via upload

--- PZ-7/PZ-7.2.py
+++ PZ-7/PZ-7.2.py
@@ -1,9 +1,11 @@
 # Даны строки S, S1 и S2. Заменить в строке S первое вхождение строки S1 на строку S2.
 try:
     S1 = input("Введите S1: ")
-    S2 = input("Введите S2: ")
     S = input("Введите S: ")
-    S = S1 + S
-    S1 in S  
+    Skull = S1 + S
+    print(Skull)
+    S2 = input("Введите S2: ")
+    Skuf = S2 + S
+    print(Skuf)
 except TypeError:
     print('Вы ввели неверное значение')
