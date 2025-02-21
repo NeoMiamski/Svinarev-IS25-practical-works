a9a1e838e33d97d6d744b363c001d94bc0a857b3
NeoMiamski <miamski.work@gmail.com>
2025-02-21 19:07:12 +0300

    Update PZ-7.2.py

--- PZ-7/PZ-7.2.py
+++ PZ-7/PZ-7.2.py
@@ -2,10 +2,13 @@
 try:
     S1 = input("Введите S1: ")
     S = input("Введите S: ")
-    Skull = S1 + S
-    print(Skull)
-    S2 = input("Введите S2: ")
-    Skuf = S2 + S
-    print(Skuf)
+    skull = S1 + S
+    print(skull)
+    lenS1 = len(S1)
+    lenSkull = len(skull)
+    RS = []
+    for i in range(lenS1, lenSkull + 1):
+        RS.append(skull[i])
+    print(RS)
 except TypeError:
     print('Вы ввели неверное значение')
