a6b66cc544448e6a60ed6706b7daec311fca4088
NeoMiamski <miamski.work@gmail.com>
2025-02-21 14:38:50 +0300

    Add files via upload

--- PZ-9/PZ-9 (2).py
+++ PZ-9/PZ-9 (2).py
@@ -2,16 +2,19 @@
 # отражающая продажи продукции по дням в кг. Преобразовать информацию из
 # строки в словари, с использованием функции найти минимальные продажи по
 # каждому виду продукции, результаты вывести на экран.
-# def minsells(sells, l):
-    
-s = ("груши 45 991 63 100 12 морковь 13 47 26 0 16")
-d = s.split()
-goods = d[0]
-sells = d[1:5]
-goods1 = d[6]
-sells1 = d[7:12]
-goods2 = goods + goods1
-sells2 = sells + sells1
-#stat = {goods: sells}
-#stat[goods] = sells
-print(goods2, sells2)
+try:
+    def minsells(A1, A2, A3, A4, A5, B1, B2, B3, B4, B5):
+        pearsells = min(A1, A2, A3, A4, A5)
+        carrotsells = min(B1, B2, B3, B4, B5)
+        return pearsells, carrotsells
+    s = ("груши 45 991 63 100 12 морковь 13 47 26 0 16")
+    d = s.split()
+    goods1, goods2 = d[0], d[6]
+    sells1, sells2 = d[1:6], d[7:12]
+    A1, A2, A3, A4, A5, B1, B2, B3, B4, B5 = int(d[1]), int(d[2]), int(d[3]), int(d[4]), int(d[5]), int(d[7]), int(d[8]), int(d[9]), int(d[10]), int(d[11])
+    production = minsells(A1, A2, A3, A4, A5, B1, B2, B3, B4, B5)
+    report = {goods1: sells1, goods2: sells2}
+    print("Минимальные продажи груш и моркови:", production, 'соответственно')
+    print(report)
+except TypeError:
+    print('Вы ввели неверное значение')
