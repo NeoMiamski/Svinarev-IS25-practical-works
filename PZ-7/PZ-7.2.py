e4fe47b2fb6487c80a2dfd39459ecb3b7ded82b2
NeoMiamski <miamski.work@gmail.com>
2025-03-15 12:49:27 +0300

    Add files via upload

--- PZ-7/PZ-7.2.py
+++ PZ-7/PZ-7.2.py
@@ -1,16 +1,12 @@
 # Даны строки S, S1 и S2. Заменить в строке S первое вхождение строки S1 на строку S2.
 try:
-    S1 = input("Введите S1: ")
     S = input("Введите S: ")
-    S_S1 = S1 + S
-    print("Вы ввели:", S_S1)
+    S1 = input("Введите S1: ")
     S2 = input("Введите S2: ")
-    lenS1 = len(S1)
-    len_S_S1 = len(S_S1)
-    S_in_list = []
-    for i in range(lenS1, len_S_S1):
-        S_in_list.append(S_S1[i])
-    print("Вы ввели:", S2 + ''.join(S_in_list))
-
+    if S1 in S:
+        Changed = S.replace(S1, S2, 1)
+        print(Changed)
+    else:
+        print("S1 не входит в строку S!")
 except TypeError:
     print('Вы ввели неверное значение')
--- PZ-9/PZ-9.py
+++ PZ-9/PZ-9.py
@@ -3,20 +3,20 @@
 # строки в словари, с использованием функции найти минимальные продажи по
 # каждому виду продукции, результаты вывести на экран.
 try:
-    def minsells(A1, A2, A3, A4, A5, B1, B2, B3, B4, B5):
-        pear_sells = min(A1, A2, A3, A4, A5)
-        carrot_sells = min(B1, B2, B3, B4, B5)
+    def min_sells(sells1, sells2):
+        pear_sells = min(sells1)
+        carrot_sells = min(sells2)
         return pear_sells, carrot_sells
-    d = "груши 45 991 63 100 12 морковь 13 47 26 0 16"
-    d = d.split()
-    goods1, goods2 = d[0], d[6]
-    A1, A2, A3, A4, A5, B1, B2, B3, B4, B5 = int(d[1]), int(d[2]), int(d[3]), int(d[4]), int(d[5]), int(d[7]), int(d[8]), int(d[9]), int(d[10]), int(d[11])
-    production = minsells(A1, A2, A3, A4, A5, B1, B2, B3, B4, B5)
-    sells1, sells2 = [A1, A2, A3, A4, A5], [B1, B2, B3, B4, B5]
-    report1 = {goods1: sells1}
-    report2 = {goods2: sells2}
-    C1, C2 = production[0], production[1]
-    print("Минимальные продажи груш:", C1, "\nМинимальные продажи и моркови:", C2)
+    report1 = {}
+    report2 = {}
+    s = "груши 45 991 63 100 12 морковь 13 47 26 0 16"
+    s = s.split()
+    sells1 = [int(s[1]), int(s[2]), int(s[3]), int(s[4]), int(s[5])]
+    sells2 = [int(s[7]), int(s[8]), int(s[9]), int(s[10]), int(s[11])]
+    production = min_sells(sells1, sells2)
+    report1[s[0]] = sells1
+    report2[s[6]] = sells2
+    print("Минимальные продажи груш:", production[0], "\nМинимальные продажи моркови:", production[1])
     print(report1, report2)
 except TypeError:
     print('Вы ввели неверное значение')
