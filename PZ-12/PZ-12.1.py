11bafdcc96e87269ca9d2cc58850d43db99fd239
NeoMiamski <miamski.work@gmail.com>
2025-04-26 12:18:46 +0300

    Add files via upload

--- PZ-12/PZ-12.1.py
+++ PZ-12/PZ-12.1.py
@@ -1,17 +1,15 @@
 # Даны значения роста 20 юношей. Определить сколько юношей будут направлены
 # в баскетбольную команду (рост от 190) и сколько в футбольную (остальные).
-
 import random
-height = []
-soccer = []
-basketball = []
+
+height, soccer, basketball = [], [], []
 for i in range(20):
-    height.append(random.randint(170, 210))
+    height.append(random.randint(150, 200))
     if height[i] < 190:
         soccer.append(height[i])
     else:
         basketball.append(height[i])
+print(len(soccer), 'юнош направлены в футбольную команду и', len(basketball), 'в баскетбольную')
 print(soccer)
 print(basketball)
-
 
