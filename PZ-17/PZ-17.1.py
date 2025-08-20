d99ef921cc26be9c3aa2f0d6e5e69864beab1039
NeoMiamski <miamski.work@gmail.com>
2025-08-20 16:08:41 +0300

    Add files via upload

--- PZ-17/PZ-17.1.py
+++ PZ-17/PZ-17.1.py
@@ -1,24 +1,83 @@
+import tkinter as tk
+from msvcrt import locking
 from tkinter import *
-from tkinter import ttk
+from tkinter import messagebox
+from tkinter import filedialog
+import pickle
 
-root = Tk()
+def open_file():
+    file_path = filedialog.askopenfilename(
+        title="Выберите файл",
+        filetypes=[("Текстовые файлы", "*.txt"), ("Все файлы", "*.*")]
+    )
+
+root = tk.Tk()
 root.title('Форма заявки') #заголовок
 root.geometry('800x600')
 
-def button_clicked():
-    print("Клик")
+form = Label(
+   root,
+   text="Форма заявки"
+)
 
-def close():
-    root.destroy()
-    root.quit()
+conditions = Label(
+   root,
+   text="Допустимые типы вложений: zip, rar, txt, doc, jpg, png, odt, xml\nМакс. размер каждого файла: 1024kb,\nМакс. общий размер файла: 2048kb."
+)
 
-button = ttk.Button(root,
-    text="Press Me",
-    command=button_clicked)
-button.pack(fill=BOTH)
+name = Label(
+   root,
+   text="Ваше имя:"
+)
 
-root.protocol('WM_DELETE_WINDOW', close)
+email = Label(
+   root,
+   text="Ваше Email:"
+)
 
+theme = Label(
+   root,
+   text="Тема письма:"
+)
 
+file1 = Label(
+   root,
+   text="Прикрепить файл:"
+)
+
+file2 = Label(
+   root,
+   text="Прикрепить файл:"
+)
+
+file3 = Label(
+   root,
+   text="Прикрепить файл:"
+)
+
+e1 = tk.Entry(root)
+e2 = tk.Entry(root)
+e3 = tk.Entry(root)
+e4 = tk.Entry(root)
+e5 = tk.Entry(root)
+
+form.grid(row=0)
+conditions.grid(row=1)
+name.grid(row=2, column=0)
+email.grid(row=3, column=0)
+theme.grid(row=4, column=0)
+file1.grid(row=5, column=0)
+file2.grid(row=6, column=0)
+file3.grid(row=7, column=0)
+
+e1.grid(row=2, column=1)
+e2.grid(row=3, column=1)
+e3.grid(row=4, column=1)
+
+open_button = tk.Button(root, text="Открыть файл", command=open_file)
+open_button.pack(pady=10)
+
+text_area = tk.Text(root)
+text_area.pack(padx=10, pady=10)
 
 root.mainloop() #отображение окна
